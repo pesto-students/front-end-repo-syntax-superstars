c868261e0fea9bfd5be94516e5e4db0584676ec3
Archana Rangrej <archana@Archanas-MacBook-Pro.local>
2023-08-16 19:11:18 +0530

    feat: Modify the api base url

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { getUser } from "../utils";
 
-const APP_BASE_URL =
-  "https://64dcc997bbd78b05d8f64417--intelliplagiarismai-backend.netlify.app/api/";
+const APP_BASE_URL = "https://intelliplagiarismai-backend.netlify.app/api/";
 
 const user = getUser();
 
