372de50ea3e68a4e044f32325a1ffc38c47ec89b
Archana Rangrej <archana@Archanas-MacBook-Pro.local>
2023-08-17 13:37:37 +0530

    feat: modify the api url

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getUser } from "../utils";
 
-const APP_BASE_URL = "https://intelliplagiarismai-backend.netlify.app/api/";
+const APP_BASE_URL = "https://intelliplagiarism-backend.onrender.com/api/";
 
 const user = getUser();
 
@@ -10,6 +10,8 @@ const instance = axios.create({
   headers: {
     Authorization: `Bearer ${user?.token}`,
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:8888/",
+    "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, PATCH, OPTIONS",
   },
 });
 
